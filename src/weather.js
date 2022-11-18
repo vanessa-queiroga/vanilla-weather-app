e4576857c4537b54131a7064212570402fa5690a
vanessa-queiroga <116520236+vanessa-queiroga@users.noreply.github.com>
2022-11-18 13:26:37 +0000

    connected open weather API to app

--- src/weather.js
+++ src/weather.js
@@ -1,1 +1,4 @@
-
+let city = "Barcelona";
+let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(formatTemperature);
